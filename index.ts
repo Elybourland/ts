7625570644fa9696c94f31e38970e106a7cf39a2
Elybourland <JEBourland@yahoo.com>
2022-01-23 13:07:24 -0800

    ts-node-dev and interfaces

---
+++ index.ts
@@ -0,0 +1,17 @@
+interface Post {
+  title: string;
+  daysOld: number;
+  published: boolean;
+}
+
+const post = {
+  title: 'Latest Typescript News',
+  daysOld: 10,
+  published: true
+};
+
+const printPost = (postToPrint: Post) => {
+  return `${postToPrint.title} (${postToPrint.daysOld})`;
+};
+
+printPost(post);
